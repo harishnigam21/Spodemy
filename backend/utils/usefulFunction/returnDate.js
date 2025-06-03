cc6d14c922dd9fa841a8a52ab008c10ad06d02d6
Harish Nigam <harishnigam21@gmail.com>
2025-06-03 09:42:18 +0530

    Update returnDate.js

--- backend/utils/usefulFunction/returnDate.js
+++ backend/utils/usefulFunction/returnDate.js
@@ -1,5 +1,6 @@
-const {format} = require("date-fns");
+const { format } = require("date-fns");
 const getDate = () => {
+  const now = new Date();
   const dateFormat = format(now, "dd-MM-yyyy HH:mm:ss");
   return dateFormat;
 };
