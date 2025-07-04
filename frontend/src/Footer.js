f70a3e8930159961f554c184ad0ded940fdfd7a1
Harish Nigam <harishnigam21@gmail.com>
2025-07-04 11:03:33 +0530

    temporary update

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -5,7 +5,7 @@ const Footer = ({appname}) =>
         const currentYear = format(new Date(),'yyyy');
         return(
             <footer className='footer'>
-                <p>copyright &copy; {currentYear} {appname}</p>
+                <p>copyright &copy; {currentYear} {appname} (Harish's Project)</p>
             </footer>
         );
     }
